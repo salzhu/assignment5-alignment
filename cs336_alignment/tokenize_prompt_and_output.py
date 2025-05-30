34d42f9bd0dc8c4f35c61adf317b8eab6a8b7189
salzhu <163059861+salzhu@users.noreply.github.com>
2025-05-30 15:58:02 -0700

    fix input ids

--- cs336_alignment/tokenize_prompt_and_output.py
+++ cs336_alignment/tokenize_prompt_and_output.py
@@ -9,6 +9,9 @@ def tokenize_prompt_and_output(prompt_strs, output_strs, tokenizer):
     texts = [prompt + output for prompt, output in zip(prompt_strs, output_strs)]
     tokenized_texts = tokenizer(texts)
 
+    print(input_ids)
+    print(input_ids.shape)
+
     input_ids = tokenized_texts['input_ids'][:,:-1]
     labels = tokenized_texts['input_ids'][:,1:]
     masks = torch.tensor([[0] * len(prompt) + [1] * len(output) for prompt, output in zip(prompt_strs, output_strs)])
