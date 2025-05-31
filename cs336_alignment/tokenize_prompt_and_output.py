88439b64ca4c0368661536b0701651a108558c45
salzhu <163059861+salzhu@users.noreply.github.com>
2025-05-30 17:19:02 -0700

    add padding

--- cs336_alignment/tokenize_prompt_and_output.py
+++ cs336_alignment/tokenize_prompt_and_output.py
@@ -9,8 +9,11 @@ def tokenize_prompt_and_output(prompt_strs, output_strs, tokenizer):
     texts = [prompt + output for prompt, output in zip(prompt_strs, output_strs)]
     tokenized_texts = tokenizer(texts)
 
-    print(torch.tensor(tokenized_texts['input_ids']))
-    print(torch.tensor(tokenized_texts['input_ids']).shape)
+    max_length = max([len(text) for text in tokenized_texts])
+    padded_texts = [text + [tokenizer.pad_token] * (max_length - len(text)) for text in tokenized_texts]
+
+    print(torch.tensor(padded_texts['input_ids']))
+    print(torch.tensor(padded_texts['input_ids']).shape)
 
     input_ids = tokenized_texts['input_ids'][:,:-1]
     labels = tokenized_texts['input_ids'][:,1:]
