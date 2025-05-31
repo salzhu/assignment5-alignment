b1dac8b3bf89cf0f4163caaaecda8bdbbc1c3257
salzhu <163059861+salzhu@users.noreply.github.com>
2025-05-31 12:28:36 -0700

    logprobs

--- cs336_alignment/sft_helper.py
+++ cs336_alignment/sft_helper.py
@@ -23,6 +23,20 @@ def tokenize_prompt_and_output(prompt_strs, output_strs, tokenizer):
 
 def compute_entropy(logits: torch.Tensor) -> torch.Tensor:
     logitnorm = torch.logsumexp(logits, dim=-1) # over the vocab 
-    print(logits.shape, logitnorm.shape)
+    # print(logits.shape, logitnorm.shape)
     logprobs = logits - torch.unsqueeze(logitnorm,dim=-1) # normalize the log 
     return torch.sum(-1 * logprobs * torch.exp(logprobs), dim=-1)
+
+def get_response_log_probs(
+    model,
+    input_ids: torch.Tensor,
+    labels: torch.Tensor,
+    return_token_entropy: bool = False,
+) -> dict[str, torch.Tensor]:
+    logits = model(input_ids).logits # batch size x seq len x vocab size
+    logprobs = torch.log(torch.nn.functional.softmax(logits, dim=-1)) #, dim=
+    print(logprobs.shape)
+
+    if not return_token_entropy:
+        return {'log_probs': logprobs}
+    return {'log_probs': logprobs, 'token_entropy': compute_entropy(logits)}
