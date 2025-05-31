f29056fe794a0e965e98b21678e68edda0e042b8
salzhu <163059861+salzhu@users.noreply.github.com>
2025-05-31 12:29:10 -0700

    adapter for get_response_log_probs

--- tests/adapters.py
+++ tests/adapters.py
@@ -8,7 +8,7 @@ from torch import Tensor
 from torch.utils.data import Dataset
 from transformers import PreTrainedTokenizerBase
 
-from cs336_alignment.sft_helper import tokenize_prompt_and_output, compute_entropy
+from cs336_alignment.sft_helper import tokenize_prompt_and_output, compute_entropy, get_response_log_probs
 
 def run_tokenize_prompt_and_output(
     prompt_strs: list[str],
@@ -117,6 +117,7 @@ def run_get_response_log_probs(
                 we have not masked out the token indices corresponding to the prompt
                 or padding; that is done in the train loop.
     """
+    return get_response_log_probs(model, input_ids, labels, return_token_entropy)
     raise NotImplementedError
 
 
