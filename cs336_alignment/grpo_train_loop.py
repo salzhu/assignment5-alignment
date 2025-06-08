be393386cca5b0714dedf1684c09d6d50f54a174
salzhu <163059861+salzhu@users.noreply.github.com>
2025-06-08 15:39:35 -0700

    r1 prompt

--- cs336_alignment/grpo_train_loop.py
+++ cs336_alignment/grpo_train_loop.py
@@ -116,7 +116,7 @@ def train_grpo(model_name,
     
     wandb.init(
         project="a5-grpo",
-        name=f"grpo_lr{learning_rate}_{loss_type}_epochs{epochs_per_rollout_batch}_bs{train_batch_size}",  # Set your run name here
+        name=f"lr{learning_rate}_{loss_type}_epochs{epochs_per_rollout_batch}_bs{train_batch_size}_len{length_normalize}_std{use_std_normalization}",  # Set your run name here
     )
     # epochs{epochs_per_rollout_batch}_bs{train_batch_size}
     # len{length_normalize}_std{use_std_normalization}
